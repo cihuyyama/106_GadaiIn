3e7ee7b4d181645fc0bcb9ffee6148609b198fec
cihuyyama <miqbalalhabib@gmail.com>
2023-06-21 22:16:32 +0700

    working on sign in with google

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gadain/view/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +14,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: Colors.deepPurple,
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.teal)),
+      home: HomePage(),
     );
   }
 }
